1105caf2f316556a812e46bec6e2c1d07ce199a7
Bruno Rodrigues <brunorb.dev@gmail.com>
2022-03-23 16:16:11 -0300

    style: alterando nome da variavel ex_05

--- Modulo_01/ex_05.py
+++ Modulo_01/ex_05.py
@@ -3,6 +3,6 @@ letras = list(palavra)
 
 for x in range(0, len(palavra), 1):
     if x <= len(palavra) - 1:
-        teste = letras[x].isdigit()
-        if teste == True:
+        descoberta_de_numeros = letras[x].isdigit()
+        if descoberta_de_numeros == True:
             print(f'Existe um número na {x + 1}ª posição da palavra')
