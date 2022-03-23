4364d492554b6f391dd272415e0f74aa631cb179
Bruno Rodrigues <brunorb.dev@gmail.com>
2022-03-23 12:45:34 -0300

    refactor: otimizando calculo de resto

--- Modulo_00/ex_medio_01.py
+++ Modulo_00/ex_medio_01.py
@@ -1,7 +1,6 @@
 numero = int(input("Digite um número para saber se ele é Par ou Ímpar: "))
-res = numero % 2
 
-if res == 0:
+if numero % 2 == 0:
     print(f'O número {numero} é PAR!')
 else:
     print(f'O número {numero} é ÍMPAR!')
