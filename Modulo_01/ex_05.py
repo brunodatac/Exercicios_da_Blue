f5e08e01b6c06839828527b30c7de8b91d75c7a8
Bruno Rodrigues <brunorb.dev@gmail.com>
2022-03-24 12:28:38 -0300

    refactor: removendo metodo

--- Modulo_01/ex_05.py
+++ Modulo_01/ex_05.py
@@ -1,8 +1,7 @@
 palavra = str(input('Digite uma palavra: ')).replace(' ','')
-letras = list(palavra)
 
 for x in range(0, len(palavra), 1):
     if x <= len(palavra) - 1:
-        descoberta_de_numeros = letras[x].isdigit()
+        descoberta_de_numeros = palavra[x].isdigit()
         if descoberta_de_numeros == True:
             print(f'Existe um número na {x + 1}ª posição da palavra')
