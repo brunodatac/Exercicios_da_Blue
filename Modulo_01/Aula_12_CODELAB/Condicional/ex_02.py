b8740065d3f7de8f0ea206a14aa80319e6d9d872
Bruno Rodrigues <brunorb.dev@gmail.com>
2022-04-05 21:47:24 -0300

    chore: adicionando exercicio ex_02

---
+++ Modulo_01/Aula_12_CODELAB/Condicional/ex_02.py
@@ -0,0 +1,8 @@
+numero = int(input('Digite um número: '))
+
+if numero > 0:
+    print(f'O número {numero} é POSITIVO')
+elif numero < 0:
+    print(f'O número {numero} é NEGATIVO')
+else:
+    print('O número digitado foi o ZERO')
